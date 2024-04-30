85a1656208eb60581f61847bec8c3d91b743e634
EliBP <52673344+EliBP@users.noreply.github.com>
2024-04-30 10:33:55 -0400

    comments and a very unrealistic wegovy treatment function

--- figure_with_wegovy.py
+++ figure_with_wegovy.py
@@ -6,7 +6,7 @@ from scipy.integrate import solve_ivp as sivp
 from scipy.interpolate import interp1d
 import string
 import matplotlib.transforms as mtransforms
-from model import *  # Importing the model definition from model.py
+from model_with_wegovy import *  # Importing the model definition from model.py
 
 # Initial values for the variables
 y0 = [0] * 8
@@ -61,40 +61,40 @@ for i in range(len(axs)-1):
         axs[i].set_ylim([0, 0.7])
     if(i == 7):
         axs[i].set_ylim([20, 45])
-        
-for i1,inci in enumerate(incivals):
-	
-	pars['inc_i1']=inci
-	pars['it1']=150
-	pars_npa=np.array(list(pars.values())) 
-	
-	sol=sivp(odde,tspan,y0,method='LSODA',args=[pars_npa])
-	sol.y[7]=sol.y[7]/(pars['height']**2)
-	t_sim=sol.t/30
 
-	# Plotting Disease progression index (DPI=si*b*sigma/(si0*b0*sigma0)
-	dp=1-sol.y[3]*sol.y[4]*sol.y[5]/(sol.y[3][0]*sol.y[4][0]*sol.y[5][0])
-	
-	fdp=interp1d(t_sim,dp)
-	tps=np.arange(0,121,12)
-	axs[8].bar(tps-3 + 3*i1, fdp(tps), color=cols[i1], width = 3)
-	axs[8].set_xlabel('Time (month)',size='medium')
-	axs[8].set_ylabel('DPI',size='medium',labelpad=2)
-	axs[8].set_xlim([t_sim[0],t_sim[-1]+5])
-	axs[8].set_ylim([0,1])
-	
-	old=0
-	
-	for i,j in enumerate(sol.y):
-		axs[i].plot(t_sim,j,c=cols[i1],label=f'DE$_i$={100+incilab[i1]}% BL',linestyle='-')
-		axs[i].set_xlabel('Time (month)',size='medium')
-		axs[i].set_ylabel(vn[i],size='medium',labelpad=2)
-		axs[i].set_xlim([min(t_sim),max(t_sim)])
-		if i==0:
-			axs[i].legend(fontsize='small',framealpha=0.5,ncol=3,loc=(0,1.25))
-			if old==0:
-				axs[i].hlines(y=[100,125],xmin=0,xmax=max(sol.t)/30,linestyles=':',colors=['k','r'],linewidth=0.8)
-				old=1
+for i1,inci in enumerate(incivals):
+    
+    pars['inc_i1']=inci
+    pars['it1']=150
+    pars_npa=np.array(list(pars.values())) 
+    
+    sol=sivp(odde,tspan,y0,method='LSODA',args=[pars_npa])
+    sol.y[7]=sol.y[7]/(pars['height']**2)
+    t_sim=sol.t/30
+
+    # Plotting Disease progression index (DPI=si*b*sigma/(si0*b0*sigma0)
+    dp=1-sol.y[3]*sol.y[4]*sol.y[5]/(sol.y[3][0]*sol.y[4][0]*sol.y[5][0])
+    
+    fdp=interp1d(t_sim,dp)
+    tps=np.arange(0,121,12)
+    axs[8].bar(tps-3 + 3*i1, fdp(tps), color=cols[i1], width = 3)
+    axs[8].set_xlabel('Time (month)',size='medium')
+    axs[8].set_ylabel('DPI',size='medium',labelpad=2)
+    axs[8].set_xlim([t_sim[0],t_sim[-1]+5])
+    axs[8].set_ylim([0,1])
+    
+    old=0
+    
+    for i,j in enumerate(sol.y):
+        axs[i].plot(t_sim,j,c=cols[i1],label=f'DE$_i$={100+incilab[i1]}% BL',linestyle='-')
+        axs[i].set_xlabel('Time (month)',size='medium')
+        axs[i].set_ylabel(vn[i],size='medium',labelpad=2)
+        axs[i].set_xlim([min(t_sim),max(t_sim)])
+        if i==0:
+            axs[i].legend(fontsize='small',framealpha=0.5,ncol=3,loc=(0,1.25))
+            if old==0:
+                axs[i].hlines(y=[100,125],xmin=0,xmax=max(sol.t)/30,linestyles=':',colors=['k','r'],linewidth=0.8)
+                old=1
 
 fig5.tight_layout()
 fig5.subplots_adjust(top=0.88,
--- model_with_wegovy.py
+++ model_with_wegovy.py
@@ -1,4 +1,4 @@
-# model.py
+# model_with_wegovy.py
 
 import numpy as np
 
@@ -38,7 +38,7 @@ pars['inc_i3'] = 0  # Increment factor for dietary intake change phase 3 (dimens
 pars['it1'] = 0  # Start time for dietary intake change phase 1 (days)
 pars['it2'] = 100000  # Start time for dietary intake change phase 2 (days)
 pars['it3'] = 100000  # Start time for dietary intake change phase 3 (days)
-pars['tau_w'] = 1.5  # Time constant for body weight dynamics (days)
+pars['tau_w'] = 1.5  # Time constant for body weight dynamics
 pars['k_infl'] = 40  # Inflammation response threshold (dimensionless)
 pars['n_infl'] = 6  # Hill coefficient for the effect of BMI on systemic inflammation (dimensionless)
 pars['infl_b'] = 0  # Baseline level of systemic inflammation (dimensionless)
@@ -84,144 +84,159 @@ pars_l = list(pars.values())  # Convert dictionary values to a list
 pars_npa = np.array(pars_l)  # Convert list to numpy array for computational efficiency
 pars_n = list(pars.keys())  # Extract dictionary keys for reference
 
+def adjust_for_wegovy(t, pars):
+    if t > 1825:  # Assuming drug introduction after three years, expressed in days
+        ramp_duration = 365  # duration over which the drug effect ramps up in days
+        ramp_factor = min((t - 1825) / ramp_duration, 1)  # caps at 1 when the full effect is reached
+        pars['inc_i1'] = max(pars['inc_i1'] - ramp_factor,  - 0.01)   # Gradually apply the reduction in caloric intake
+        # pars['tau_w'] += ramp_factor # 
+
 def odde(t,y,pars_npa): 
-	heav=lambda x: np.heaviside(x,1) 
+    heav=lambda x: np.heaviside(x,1) 
+    pars = dict(zip(pars_n, pars_npa))
+
+    # Adjust parameters based on the drug effects if applicable
+    adjust_for_wegovy(t, pars)
+
+    # Update pars_npa with potentially modified values from pars
+    pars_npa = np.array(list(pars.values()))
+
  #Initial Values 
-	g=y[0]
-	i=y[1]
-	ffa=y[2]
-	si=y[3]
-	b=y[4]
-	sigma=y[5]
-	infl=y[6]
-	w=y[7]
+    g=y[0]
+    i=y[1]
+    ffa=y[2]
+    si=y[3]
+    b=y[4]
+    sigma=y[5]
+    infl=y[6]
+    w=y[7]
 
  #Parameter Values 
-	eg0=pars_npa[0]
-	k=pars_npa[1]
-	bv=pars_npa[2]
-	mmax=pars_npa[3]
-	alpha_m=pars_npa[4]
-	km=pars_npa[5]
-	alpha_isr=pars_npa[6]
-	kisr=pars_npa[7]
-	pmax=pars_npa[8]
-	kp=pars_npa[9]
-	alpha_p=pars_npa[10]
-	p_b=pars_npa[11]
-	amax=pars_npa[12]
-	alpha_a=pars_npa[13]
-	ka=pars_npa[14]
-	a_b=pars_npa[15]
-	tau_b=pars_npa[16]
-	height=pars_npa[17]
-	age_b=pars_npa[18]
-	sex=pars_npa[19]
-	cage=pars_npa[20]
-	target_si=pars_npa[21]
-	tau_si=pars_npa[22]
-	bmi_h=pars_npa[23]
-	mffa=pars_npa[24]
-	ksi_infl=pars_npa[25]
-	ksi_ffa=pars_npa[26]
-	nsi_ffa=pars_npa[27]
-	sw11=pars_npa[28]
-	inc_i1=pars_npa[29]
-	inc_i2=pars_npa[30]
-	inc_i3=pars_npa[31]
-	it1=pars_npa[32]
-	it2=pars_npa[33]
-	it3=pars_npa[34]
-	tau_w=pars_npa[35]
-	k_infl=pars_npa[36]
-	n_infl=pars_npa[37]
-	infl_b=pars_npa[38]
-	tau_infl=pars_npa[39]
-	hgp_bas=pars_npa[40]
-	hepa_max=pars_npa[41]
-	hepa_sic=pars_npa[42]
-	a_hgp=pars_npa[43]
-	gcg=pars_npa[44]
-	k_gcg=pars_npa[45]
-	s=pars_npa[46]
-	png=pars_npa[47]
-	sigma_b=pars_npa[48]
-	sgmu=pars_npa[49]
-	sgmd=pars_npa[50]
-	sfm=pars_npa[51]
-	sim=pars_npa[52]
-	sgku=pars_npa[53]
-	sgkd=pars_npa[54]
-	sfk=pars_npa[55]
-	sik=pars_npa[56]
-	nsgku=pars_npa[57]
-	nsgkd=pars_npa[58]
-	nsfk=pars_npa[59]
-	nsik=pars_npa[60]
-	tau_sigma=pars_npa[61]
-	sci=pars_npa[62]
-	gclamp=pars_npa[63]
-	iclamp=pars_npa[64]
-	maxinfl=pars_npa[65]
-	l0=pars_npa[66]
-	l2=pars_npa[67]
-	cf=pars_npa[68]
-	ksif=pars_npa[69]
-	aa=pars_npa[70]
-	sif=pars_npa[71]
-	ffa_ramax=pars_npa[72]
-	lp1=pars_npa[73]
-	lp2=pars_npa[74]
-	tau_wsc=pars_npa[75]
-	
+    eg0=pars_npa[0]
+    k=pars_npa[1]
+    bv=pars_npa[2]
+    mmax=pars_npa[3]
+    alpha_m=pars_npa[4]
+    km=pars_npa[5]
+    alpha_isr=pars_npa[6]
+    kisr=pars_npa[7]
+    pmax=pars_npa[8]
+    kp=pars_npa[9]
+    alpha_p=pars_npa[10]
+    p_b=pars_npa[11]
+    amax=pars_npa[12]
+    alpha_a=pars_npa[13]
+    ka=pars_npa[14]
+    a_b=pars_npa[15]
+    tau_b=pars_npa[16]
+    height=pars_npa[17]
+    age_b=pars_npa[18]
+    sex=pars_npa[19]
+    cage=pars_npa[20]
+    target_si=pars_npa[21]
+    tau_si=pars_npa[22]
+    bmi_h=pars_npa[23]
+    mffa=pars_npa[24]
+    ksi_infl=pars_npa[25]
+    ksi_ffa=pars_npa[26]
+    nsi_ffa=pars_npa[27]
+    sw11=pars_npa[28]
+    inc_i1=pars_npa[29]
+    inc_i2=pars_npa[30]
+    inc_i3=pars_npa[31]
+    it1=pars_npa[32]
+    it2=pars_npa[33]
+    it3=pars_npa[34]
+    tau_w=pars_npa[35]
+    k_infl=pars_npa[36]
+    n_infl=pars_npa[37]
+    infl_b=pars_npa[38]
+    tau_infl=pars_npa[39]
+    hgp_bas=pars_npa[40]
+    hepa_max=pars_npa[41]
+    hepa_sic=pars_npa[42]
+    a_hgp=pars_npa[43]
+    gcg=pars_npa[44]
+    k_gcg=pars_npa[45]
+    s=pars_npa[46]
+    png=pars_npa[47]
+    sigma_b=pars_npa[48]
+    sgmu=pars_npa[49]
+    sgmd=pars_npa[50]
+    sfm=pars_npa[51]
+    sim=pars_npa[52]
+    sgku=pars_npa[53]
+    sgkd=pars_npa[54]
+    sfk=pars_npa[55]
+    sik=pars_npa[56]
+    nsgku=pars_npa[57]
+    nsgkd=pars_npa[58]
+    nsfk=pars_npa[59]
+    nsik=pars_npa[60]
+    tau_sigma=pars_npa[61]
+    sci=pars_npa[62]
+    gclamp=pars_npa[63]
+    iclamp=pars_npa[64]
+    maxinfl=pars_npa[65]
+    l0=pars_npa[66]
+    l2=pars_npa[67]
+    cf=pars_npa[68]
+    ksif=pars_npa[69]
+    aa=pars_npa[70]
+    sif=pars_npa[71]
+    ffa_ramax=pars_npa[72]
+    lp1=pars_npa[73]
+    lp2=pars_npa[74]
+    tau_wsc=pars_npa[75]
+    
 #Numerics 
-	nsi_infl=6
-	bmi = w/height**2
-	age = age_b+cage*t/365
-	tsi1 = target_si*(ksi_infl/(ksi_infl+infl))*(1-mffa*ffa**nsi_ffa/(ffa**nsi_ffa+ksi_ffa**nsi_ffa))
-	tsi2 = target_si*(ksi_infl**nsi_infl/(ksi_infl**nsi_infl+infl**nsi_infl))*(1-mffa*ffa**nsi_ffa/(ffa**nsi_ffa+ksi_ffa**nsi_ffa))
-	tsi = sw11*tsi1+(1-sw11)*tsi2
-	intake_i = 2500
-	inc_int =  0 + heav(t-it1)*heav(it2-t)*heav(it3-t)*inc_i1 + heav(t-it2)*heav(it3-t)*inc_i2 + heav(t-it3)*inc_i3
-	
-	if t>=it2:
-		tau_w=tau_w*tau_wsc
-	
-	w_base = 81
-	expd = 2500/(7700*w_base)
-	k_w = 1/7700
-	m = mmax*g**km/(alpha_m**km + g**km)
-	isr =  sigma*(m)**kisr/(alpha_isr**kisr + (m)**kisr)
-	p_isr = p_b + pmax*isr**kp/(alpha_p**kp + isr**kp)
-	a_m = a_b + amax*m**ka/(alpha_a**ka + m**ka)
-	p =  p_isr
-	a =  a_m
-	fmass_p = 1.2*bmi + 0.23*age - 16.2
-	fmass = w*fmass_p/100
-	inc = 1+inc_int
-	hepa_si = hepa_sic*si
-	hgp = (hgp_bas + hepa_max*(a_hgp+k_gcg*gcg)/((a_hgp+k_gcg*gcg)+hepa_si*i))
-	intake = intake_i*inc
-	s_glucu = sgmu*g**nsgku/(g**nsgku+sgku**nsgku)
-	s_glucd = sgmd*g**nsgkd/(g**nsgkd+sgkd**nsgkd)
-	s_ffa = sfm*ffa**nsfk/(ffa**nsfk+sfk**nsfk)
-	s_infl = sim*infl**nsik/(infl**nsik+sik**nsik)
-	s_inf = sigma_b*(s_glucu - s_glucd*s_ffa - s_infl)
-	siff = si*sif
-	cl0 = l0*24*60/bv
-	cl2 = l2*24*60/bv
-	al0 = 2.45*24*60
+    nsi_infl=6
+    bmi = w/height**2
+    age = age_b+cage*t/365
+    tsi1 = target_si*(ksi_infl/(ksi_infl+infl))*(1-mffa*ffa**nsi_ffa/(ffa**nsi_ffa+ksi_ffa**nsi_ffa))
+    tsi2 = target_si*(ksi_infl**nsi_infl/(ksi_infl**nsi_infl+infl**nsi_infl))*(1-mffa*ffa**nsi_ffa/(ffa**nsi_ffa+ksi_ffa**nsi_ffa))
+    tsi = sw11*tsi1+(1-sw11)*tsi2
+    intake_i = 2500
+    inc_int =  0 + heav(t-it1)*heav(it2-t)*heav(it3-t)*inc_i1 + heav(t-it2)*heav(it3-t)*inc_i2 + heav(t-it3)*inc_i3
+    
+    if t>=it2:
+        tau_w=tau_w*tau_wsc
+    
+    w_base = 81
+    expd = 2500/(7700*w_base)
+    k_w = 1/7700
+    m = mmax*g**km/(alpha_m**km + g**km)
+    isr =  sigma*(m)**kisr/(alpha_isr**kisr + (m)**kisr)
+    p_isr = p_b + pmax*isr**kp/(alpha_p**kp + isr**kp)
+    a_m = a_b + amax*m**ka/(alpha_a**ka + m**ka)
+    p =  p_isr
+    a =  a_m
+    fmass_p = 1.2*bmi + 0.23*age - 16.2
+    fmass = w*fmass_p/100
+    inc = 1+inc_int
+    hepa_si = hepa_sic*si
+    hgp = (hgp_bas + hepa_max*(a_hgp+k_gcg*gcg)/((a_hgp+k_gcg*gcg)+hepa_si*i))
+    intake = intake_i*inc
+    s_glucu = sgmu*g**nsgku/(g**nsgku+sgku**nsgku)
+    s_glucd = sgmd*g**nsgkd/(g**nsgkd+sgkd**nsgkd)
+    s_ffa = sfm*ffa**nsfk/(ffa**nsfk+sfk**nsfk)
+    s_infl = sim*infl**nsik/(infl**nsik+sik**nsik)
+    s_inf = sigma_b*(s_glucu - s_glucd*s_ffa - s_infl)
+    siff = si*sif
+    cl0 = l0*24*60/bv
+    cl2 = l2*24*60/bv
+    al0 = 2.45*24*60
 
 #Diferetial Equations 
-	dg=gclamp+hgp-(eg0+sci*si*i)*g
-	di=iclamp+b*isr/bv-k*i
-	dffa=(cl0+cl2*fmass)*(ffa_ramax*(ksif**aa)/(ksif**aa+(siff*i)**aa))-cf*w*ffa
-	dsi=(tsi-si)/tau_si
-	db=(png+p*b-a*b-s*b**2)/tau_b
-	dsigma=(s_inf-sigma)/tau_sigma
-	dinfl=infl_b+(maxinfl*bmi**n_infl/(bmi**n_infl+k_infl**n_infl)-infl)/tau_infl
-	dw=(k_w*intake-expd*w)/tau_w
+    dg=gclamp+hgp-(eg0+sci*si*i)*g
+    di=iclamp+b*isr/bv-k*i
+    dffa=(cl0+cl2*fmass)*(ffa_ramax*(ksif**aa)/(ksif**aa+(siff*i)**aa))-cf*w*ffa
+    dsi=(tsi-si)/tau_si
+    db=(png+p*b-a*b-s*b**2)/tau_b
+    dsigma=(s_inf-sigma)/tau_sigma
+    dinfl=infl_b+(maxinfl*bmi**n_infl/(bmi**n_infl+k_infl**n_infl)-infl)/tau_infl
+    dw=(k_w*intake-expd*w)/tau_w
 
-	dy=np.array([dg,di,dffa,dsi,db,dsigma,dinfl,dw])
+    dy=np.array([dg,di,dffa,dsi,db,dsigma,dinfl,dw])
 
-	return dy
+    return dy
